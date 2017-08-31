b48edecfbeb463a3d225ae9bc126484d9b06379d
polinankoleva <polina.n.koleva@gmail.com>
2017-08-31 13:59:46 +0200

    Add @KafkaConfig. Resolver Consumer exception.

---
+++ src/main/java/org/aerogear/gsoc/demo/rest/KafkaClusterConfig.java
@@ -0,0 +1,8 @@
+package org.aerogear.gsoc.demo.rest;
+
+import net.wessendorf.kafka.cdi.annotation.KafkaConfig;
+
+@KafkaConfig(bootstrapServers = "localhost:9092")
+public class KafkaClusterConfig {
+
+}
--- src/main/java/org/aerogear/gsoc/demo/rest/TopicConsumers.java
+++ src/main/java/org/aerogear/gsoc/demo/rest/TopicConsumers.java
@@ -4,22 +4,21 @@ import net.wessendorf.kafka.cdi.annotation.Consumer;
 
 import java.util.logging.Logger;
 
-
 public class TopicConsumers {
 
     Logger logger = Logger.getLogger(TopicConsumers.class.getName());
 
-    //@Consumer(topics = "successMessagesPerJob", groupId = "consumer-group-1")
+    @Consumer(topics = "successMessagesPerJob", groupId = "consumer-group-1")
     public void totalSuccesses(final String jobId, final Long total) {
         logger.info("Total successes for jobId " + jobId + ": " + total);
     }
 
-    //@Consumer(topics = "failedMessagesPerJob", groupId = "consumer-group-2")
+    @Consumer(topics = "failedMessagesPerJob", groupId = "consumer-group-2")
     public void totalFailures(final String jobId, final Long total) {
         logger.info("Total failures for jobId " + jobId + ": " + total);
     }
 
-    //@Consumer(topics = "totalMessagesPerJob", groupId = "consumer-group-3")
+    @Consumer(topics = "totalMessagesPerJob", groupId = "consumer-group-3")
     public void totalMessages(final String jobId, final Long total) {
         logger.info("Total messages for jobId " + jobId + ": " + total);
     }
