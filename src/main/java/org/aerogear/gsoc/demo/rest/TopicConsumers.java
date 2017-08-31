6d18e1f657c544e38ea5fdcd04efeef5b8ad295d
polinankoleva <polina.n.koleva@gmail.com>
2017-08-31 13:42:55 +0200

    Add content of the beans file -> resolve the producer exception. New exception with a consumer thrown.

--- src/main/java/org/aerogear/gsoc/demo/rest/TopicConsumers.java
+++ src/main/java/org/aerogear/gsoc/demo/rest/TopicConsumers.java
@@ -9,17 +9,17 @@ public class TopicConsumers {
 
     Logger logger = Logger.getLogger(TopicConsumers.class.getName());
 
-    @Consumer(topics = "successMessagesPerJob", groupId = "consumer-group-1")
+    //@Consumer(topics = "successMessagesPerJob", groupId = "consumer-group-1")
     public void totalSuccesses(final String jobId, final Long total) {
         logger.info("Total successes for jobId " + jobId + ": " + total);
     }
 
-    @Consumer(topics = "failedMessagesPerJob", groupId = "consumer-group-2")
+    //@Consumer(topics = "failedMessagesPerJob", groupId = "consumer-group-2")
     public void totalFailures(final String jobId, final Long total) {
         logger.info("Total failures for jobId " + jobId + ": " + total);
     }
 
-    @Consumer(topics = "totalMessagesPerJob", groupId = "consumer-group-3")
+    //@Consumer(topics = "totalMessagesPerJob", groupId = "consumer-group-3")
     public void totalMessages(final String jobId, final Long total) {
         logger.info("Total messages for jobId " + jobId + ": " + total);
     }
